17180b24dde1e23f219072d7adb932664598b886
shinilv <19510296826@163.com>
2026-06-19 20:33:16 +0800

    hot_二叉树的中序遍历

--- lc_hot_100/二叉树的中序遍历.cpp
+++ lc_hot_100/二叉树的中序遍历.cpp
@@ -2,8 +2,22 @@
 题意：
 给定一个二叉树根节点，返回它的中序遍历
 思路：
-
+左 - 根 - 右
 */
 
-
-
+class Solution {
+public:
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        auto dfs = [&](auto&& dfs, TreeNode* node) -> void {
+            if (node == nullptr) {
+                return;
+            }
+            dfs(dfs, node->left);
+            ans.push_back(node->val);
+            dfs(dfs, node->right);
+        };
+        dfs(dfs, root);
+        return ans;
+    }
+};
