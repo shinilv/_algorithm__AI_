e61ba6c4521a048a6c340b1c47da159d6f838184
suntianci1 <suntianci@zwpim.com>
2026-06-24 11:21:56 +0800

    hot_100 零钱兑换，完全背包

--- lc_hot_100/动态规划/零钱兑换.cpp
+++ lc_hot_100/动态规划/零钱兑换.cpp
@@ -3,11 +3,20 @@
 请你计算并返回可以凑成总金额所需的最少硬币个数。如果没有任何一种硬币组合能组成总金额，返回 -1。
 你可以认为每种硬币的数量是无限的。
 
+思路：
 */
-
 class Solution {
 public:
     int coinChange(vector<int>& coins, int amount) {
-
+        int n = coins.size();
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+        for (int i = 0; i < n; i++) {
+            for (int j = coins[i]; j <= amount; j++) {
+                dp[j] = min(dp[j], dp[j - coins[i]] + 1);
+            }
+        }
+        if (dp[amount] > amount) return -1;
+        return dp[amount];
     }
 };
