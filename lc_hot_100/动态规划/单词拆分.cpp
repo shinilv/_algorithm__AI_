c28b1fb712ad254d9fff2595f6d6675ceeaae021
suntianci1 <suntianci@zwpim.com>
2026-06-26 16:46:59 +0800

    hot_100 单词拆分_线性dp

--- lc_hot_100/动态规划/单词拆分.cpp
+++ lc_hot_100/动态规划/单词拆分.cpp
@@ -10,10 +10,29 @@ s 和 wordDict[i] 仅由小写英文字母组成
 wordDict 中的所有字符串互不相同
 
 思路：
+令dp[i] 表示 s[0 - i] 是否可以被拼接
+
 */
 
 class Solution {
 public:
     bool wordBreak(string s, vector<string>& wordDict) {
+        int n = s.size(), mx = 20;
+        unordered_set<string> se;
+        for (auto x : wordDict) {
+            mx = max(int(x.size()), mx);
+            se.insert(x);
+        }
+        vector<bool> dp(n + 1, false);
+        dp[0] = true;
+        for (int i = 0; i < n; i++) {
+            for (int j = i; j >= 0 && j >= i - mx; j--) {
+                if (se.count(s.substr(j, i - j + 1)) && dp[j]) {
+                    dp[i + 1] = true;
+                    break;
+                }
+            }
+        }
+        return dp[n];
     }
 };
