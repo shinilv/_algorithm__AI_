d7f332c688a2e8c71a4651e93a7c938c4b287300
suntianci1 <suntianci@zwpim.com>
2026-06-30 19:34:49 +0800

    hot_100 最长公共子序列

---
+++ lc_hot_100/多维动态规划/最长公共子序列.cpp
@@ -0,0 +1,35 @@
+/*
+题意：
+给定两个字符串 text1 和 text2，返回这两个字符串的最长 公共子序列 的长度。如果不存在 公共子序列 ，返回 0 。
+一个字符串的 子序列 是指这样一个新的字符串：
+它是由原字符串在不改变字符的相对顺序的情况下删除某些字符（也可以不删除任何字符）后组成的新字符串。
+例如，"ace" 是 "abcde" 的子序列，但 "aec" 不是 "abcde" 的子序列。
+两个字符串的 公共子序列 是这两个字符串所共同拥有的子序列。
+
+思路：
+n, m = len(text1), len(text2)
+定义状态 dp[i][j] 表示text1[0 - i] 和 text2[0 - j] 的最长公共子序列
+
+思考状态转移方程
+if (text1[i] == text2[j]) dp[i][j] = dp[i - 1][j - 1] + 1
+else dp[i][j] = dp[i - 1][j], dp[i][j - 1]
+*/
+
+
+class Solution {
+public:
+    int longestCommonSubsequence(string text1, string text2) {
+        int n = text1.size(), m = text2.size();
+        vector<vector<int>> dp(n + 1, vector<int>(m + 1));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (text1[i] == text2[j]) {
+                    dp[i + 1][j + 1] = dp[i][j] + 1;
+                } else {
+                    dp[i + 1][j + 1] = max(dp[i + 1][j], dp[i][j + 1]);
+                }
+            }
+        }
+        return dp[n][m];
+    }
+};
